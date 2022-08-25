1f859d2751629317fe6320ee971580753154db9a
Leonardo Porto <leonardo.porto.souza@gmail.com>
2022-08-25 11:33:48 -0300

    feat: configuracao atraves do env e adicionando mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseModule],
 })
 export class AppModule {}
